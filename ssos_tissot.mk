3195bf6ee1fa037812c3a66004a9b1116ba99034
Unknownbitch07 <sufiyanmp07@gmail.com>
2021-01-04 16:15:15 +0000

    tissot: To fix the Mobile Data not works in default

--- ssos_tissot.mk
+++ ssos_tissot.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/ssos/config/common.mk)
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
